4843e75e5c4e6ddc37f9e4ed77fd6230288bcd49
Nicholas <91276870+NicholasCloud4@users.noreply.github.com>
2024-12-09 13:15:22 -0500

    first commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+import styles from './Header.module.css'
+
+export default function Header() {
+    return (
+        <header>
+            <img src="src/assets/chef-claude-icon.png" alt="chef claude icon" />
+            <h1>Chef Claude</h1>
+        </header>
+    )
+}
