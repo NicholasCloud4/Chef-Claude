dd5e6fda8cc37e506ca7d279d04e219f6f170f52
Nicholas <91276870+NicholasCloud4@users.noreply.github.com>
2024-12-13 19:16:55 -0500

    Refactored to seperate components

---
+++ src/components/IngredientsList.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import styles from "./Main.module.css"
+
+export default function IngredientsList(props) {
+
+    const ingredientsListItems = props.ingredients.map(ingredient => (
+        <li key={ingredient}>{ingredient}</li>
+    ))
+
+    return (
+        <section>
+            <h2>Ingredients on hand:</h2>
+            <ul className={styles.ingredientsList} aria-live="polite">{ingredientsListItems}</ul>
+            {props.ingredients.length >= 4 ? <div className={styles.getRecipeContainer}>
+                <div>
+                    <h3>Ready for a recipe?</h3>
+                    <p>Generate a recipe from your list of ingredients.</p>
+                </div>
+                <button onClick={props.toggleRecipeShown}>Get a recipe</button>
+            </div> : <h3>Add at least 4 ingredients to get started!</h3>}
+
+        </section>
+
+    )
+}
