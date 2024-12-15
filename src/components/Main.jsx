007da11d39b39d605a67851875e9624bac600dbd
Nicholas <91276870+NicholasCloud4@users.noreply.github.com>
2024-12-15 11:05:06 -0500

    Checking to see if i get the ai information into the console

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -2,33 +2,11 @@ import React from 'react'
 import styles from "./Main.module.css"
 import ClaudeRecipe from './ClaudeRecipe'
 import IngredientsList from './IngredientsList'
+import { getRecipeFromChefClaude, getRecipeFromMistral } from "./ai"
 
 export default function Main() {
 
-    /**
-     * Challenge: clean up our code!
-     * Let's make a couple new components to make things a
-     * little cleaner. (Notice: I'm not suggesting what we
-     * have now is bad or wrong. I'm mostly finding an excuse
-     * to get in some hands-on practice 🙂)
-     * 
-     * 1. Move the entire recipe <section> into its own
-     *    ClaudeRecipe component
-     * 2. Move the list of ingredients <section> into its
-     *    own IngredientsList component.
-     * 
-     * While you're considering how to structure things, consider
-     * where state is, think about if it makes sense or not to
-     * move it somewhere else, how you'll communicate between
-     * the parent/child components, etc.
-     * 
-     * The app should function as it currently does when you're
-     * done, so there will likely be some extra work to be done
-     * beyond what I've listed above.
-     */
-
-    const [ingredients, setIngredients] = React.useState([])
-
+    const [ingredients, setIngredients] = React.useState(["all the main spices", "pasta", "ground beef", "tomato paste"])
     const [recipeShown, setRecipeShown] = React.useState(false)
 
 
@@ -38,10 +16,9 @@ export default function Main() {
         setIngredients(prevIngredients => [...prevIngredients, newIngredient])
     }
 
-    function toggleRecipeShown() {
-        setRecipeShown((prevRecipeShown) => {
-            return (!prevRecipeShown)
-        })
+    async function getRecipe() {
+        const recipeMarkdown = await getRecipeFromMistral(ingredients)
+        console.log(recipeMarkdown)
     }
 
     return (
@@ -58,7 +35,7 @@ export default function Main() {
 
             {
                 ingredients.length > 0 ?
-                    <IngredientsList ingredients={ingredients} toggleRecipeShown={toggleRecipeShown} />
+                    <IngredientsList ingredients={ingredients} getRecipe={getRecipe} />
                     :
                     <h3>Add some ingredients to get started!</h3>
             }
